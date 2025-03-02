033b13ae4a46895a3cc0ae779be377b7ce96b294
luisarevalo21 <arevaloluis89@yahoo.com>
2025-03-01 21:16:05 -0800

    created the register page with a placeholder image and place holder text. created a grid layout following the wireframe. lastly, created routing to navigate to this page

---
+++ front-end/src/pages/Register.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import styled from "@mui/system/styled";
+
+import placeholder from "../assets/placeholder.jpg";
+
+const Item = styled("div")(({ theme }) => ({
+  backgroundColor: "#fff",
+  border: "1px solid",
+  borderColor: "#ced7e0",
+  padding: theme.spacing(1),
+  borderRadius: "4px",
+  textAlign: "center",
+  ...theme.applyStyles("dark", {
+    backgroundColor: "#1A2027",
+    borderColor: "#444d58",
+  }),
+}));
+const Register = () => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container>
+        <Grid size={{ xs: 6, md: 6 }}>
+          <img src={placeholder} alt="" width={"55%"} />
+        </Grid>
+        <Grid size={{ xs: 6, md: 6 }}>
+          <Typography>Register here!</Typography>
+          <Typography>add register componet here!</Typography>{" "}
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Register;
