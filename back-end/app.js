59cb1c71186442075fddf37f144ea2d26fc0ec39
hdkassamali <122648642+hdkassamali@users.noreply.github.com>
2025-03-03 21:45:35 -0800

    Added port 4000 to app.js

--- back-end/app.js
+++ back-end/app.js
@@ -17,4 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.listen(4000, () => {
+  console.log("Server is running on localhost:4000, you better catch it!");
+});
+
 module.exports = app;
