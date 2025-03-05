3fd2c1fcfa701b5469c9635cf07ab6463657f626
Anthony Weathers <anthonyweathers115@gmail.com>
2025-03-05 13:55:38 -0800

    Added comment for bycrypt import

--- back-end/config/passportConfig.js
+++ back-end/config/passportConfig.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs"); //for password hashing security later on
 
 passport.use(
   new LocalStrategy({ usernameField: "email" }, async function(email, password, cb) {
