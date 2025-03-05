34f4c3e5dd908f9d674b0937eacb550ac3542f33
Anthony Weathers <anthonyweathers115@gmail.com>
2025-03-05 13:21:46 -0800

    Removed some commented code and success message, only returning user object

--- back-end/config/passportConfig.js
+++ back-end/config/passportConfig.js
@@ -5,16 +5,6 @@ const bcrypt = require("bcryptjs");
 passport.use(
   new LocalStrategy({ usernameField: "email" }, async function(email, password, cb) {
     try {
-      //Placeholder for retrieving a user based off email, would need to be the data source used by the app
-      //Example provided by typeorm documentation as one way for getting data
-      //const myDataSource = new DataSource(/*...*/)
-      //const user = await myDataSource.manager.findOneBy(Admin, {
-      //  email: email,
-      //})
-      //if (!user) return cb(null, false, { message: "User not found" });
-
-      //const isMatch = await bcrypt.compare(password, user.password);
-
       //Example user to test login, ensure email and password match
       const user = {"id": 1, "email": "test@test.com", "password": "test", "username": "Charles"};
 
@@ -36,7 +26,7 @@ passport.use(
 //passport.serializeUser((user, cb) => cb(null, user.id));
 passport.serializeUser( function(user, cb) {
   console.log("Serializing user:", user);
-  cb(null, { id: user.id, username: user.username }) //stores user id and their username, if we use usernames for admins, as an object in session
+  cb(null, user) //stores user id and their username, if we use usernames for admins, as an object in session
 });
 
 //get user info from database using the stored user id
--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -7,12 +7,12 @@ router.post("/login", function(req, res, next) {
   passport.authenticate("local", (err, user, info, status) => {
     //does handling based off the returned values from local strategy;
     //info is for returned information added as a third parameter to callback in local strategy
-    //status is optional for returned status codes from local strategy as fourth parameter
+    //status is optional for returned status codes from local strategy as
     if (err) return next(err);
     if (!user) return res.status(401).json({ message: info.message });
 
     req.logIn(user, function() {
-      return res.json({ message: "Login successful", user });
+      return res.json(user)
     });
   })(req, res, next); // pass in req, res, and next to the passport local strategy function
 });
