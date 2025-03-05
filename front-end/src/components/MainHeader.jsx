5c1f07c191dd7b373e225b371abed66496bc786d
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2025-03-04 20:18:08 -0600

    Added react boilerplate in MainHeader.jsx file. Added logo.png.

---
+++ front-end/src/components/MainHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+//Material UI imports
+import { TextField, IconButton } from '@mui/material';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+//import logo(we don't have one yet so it's just a screenshot that says "logo")
+import logo from '../assets/logo.png';
+
+const MainHeader = () => {
+    
+}
