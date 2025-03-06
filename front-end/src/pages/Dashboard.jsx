b57fcaf6dd15b01e5134e48453477f44c3b9f956
luisarevalo21 <arevaloluis89@yahoo.com>
2025-03-05 21:34:51 -0800

    created dashboard layout boiler plate and routing existed already for dashboard

--- front-end/src/pages/Dashboard.jsx
+++ front-end/src/pages/Dashboard.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+
 const Dashboard = () => {
   return (
-    <Box>
+    <>
       <Typography> hello from Dashboard page!</Typography>
-    </Box>
+
+      <Grid container spacing={2}>
+        <Grid size={6}>
+          <Box p={10} border={"1px solid black"} bgcolor={"#f5f5f5"}>
+            <Typography>hello from left box</Typography>
+          </Box>
+        </Grid>
+
+        <Grid size={6}>
+          <Box p={10} border={"1px solid black"} bgcolor={"#f5f5f5"}>
+            <Typography>hello from right box</Typography>
+          </Box>
+        </Grid>
+        <Grid size={12}>
+          <Box p={10} border={"1px solid black"} bgcolor={"#f5f5f5"}>
+            <Typography>hello from bottom box</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
