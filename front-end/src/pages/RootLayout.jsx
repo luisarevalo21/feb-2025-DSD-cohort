33f08190bacef48262dde4fc4799796b5bb277e6
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2025-03-05 22:23:53 -0600

    Made changes to rootlayout which work, but I may want to simplify. Modified app.jsx accordingly, changed index from homepagelogin to dashboard, separated public and protected routes, etc.

--- front-end/src/pages/RootLayout.jsx
+++ front-end/src/pages/RootLayout.jsx
@@ -6,12 +6,22 @@ import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
+      {/* Header section  */}
       <MainHeader />
-      <main>
-        <Outlet />
-      </main>
-    </>
+      
+      {/* Main content section  */}
+      <div className="flex flex-1">
+        {/* Side navigation(placeholder for now)  */}
+        <aside className="w-64 bg-blue-200 p-4">
+          <div className="text-center font-semibold">Side nav placeholder</div>
+        </aside>
+        {/* Content for the page  */}
+        <main className="flex-1 p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 };
 
