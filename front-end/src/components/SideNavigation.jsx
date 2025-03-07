118405055d88aab4c186d69d227765ff0d123731
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2025-03-06 21:49:34 -0600

    It's rendering, so far to the very left of the page

--- front-end/src/components/SideNavigation.jsx
+++ front-end/src/components/SideNavigation.jsx
@@ -11,3 +11,59 @@ import {
     Divider,
     Tooltip
 } from '@mui/material';
+//Icons
+import MenuIcon from '@mui/icons-material/Menu';
+import DashboardIcon from '@mui/icons-material/DashBoard'
+import LockIcon from '@mui/icons-material/Lock';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import ReportIcon from '@mui/icons-material/Report';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { NavLink } from 'react-router-dom';
+
+//fully expanded sidebar width
+const DRAWER_WIDTH = 240;
+
+
+const SideNavigation = () => {
+    //open by default for now
+    const [open, setOpen] = useState(true);
+
+    //Handler for toggling expanded or collapsed
+    const handleToggle = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <Drawer
+          variant="permanent"
+          open={open}
+          sx={{
+            width: open ? DRAWER_WIDTH : 64,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: open ? DRAWER_WIDTH : 64,
+              overflowX: "hidden",
+              trainsition: "width 0.3s",
+            },
+          }}
+        >
+            {/* Top area with hamburger icon  */}
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: open ? "flex-end" : "center",
+                p:1
+              }}
+            >
+                <IconButton onClick={handleToggle}>
+                    <MenuIcon />
+                </IconButton>
+            </Box>
+        </Drawer>
+    )
+}
+
+
+export default SideNavigation;
