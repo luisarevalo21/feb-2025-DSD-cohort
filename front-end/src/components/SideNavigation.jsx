c281919d3896681ed9ee2ed8a4a371aeb0b94399
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2025-03-06 21:09:52 -0600

    Added SideNavigation.jsx

---
+++ front-end/src/components/SideNavigation.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import {
+    Drawer,
+    Box,
+    IconButton,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    Divider,
+    Tooltip
+} from '@mui/material';
