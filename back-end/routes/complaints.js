82869d4ffe541df0675d2f25d1b7044cc30e4765
227Faddi <falukhouma27@gmail.com>
2025-03-13 18:41:24 -0400

    fix: add error handler for complaints route

--- back-end/routes/complaints.js
+++ back-end/routes/complaints.js
@@ -5,6 +5,11 @@ const Complaint = require("../database/entities/complaint");
 
 router.get("/", async function (req, res) {
   const complaints = await AppDataSource.manager.find(Complaint);
+
+  if(!complaints || !Array.isArray(complaints)){
+    return res.status(500).json({ message: 'Database error occurred'});
+  }
+
   return res.status(200).json(complaints);
 });
 
