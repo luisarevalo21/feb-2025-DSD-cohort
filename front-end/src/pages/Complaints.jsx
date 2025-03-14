6a34cbf146ae5bcc223cb4c925b3f9c7bc1d0467
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2025-03-14 01:52:44 -0500

    Hooked up with API, still not ready for merge quite yet.

--- front-end/src/pages/Complaints.jsx
+++ front-end/src/pages/Complaints.jsx
@@ -1,9 +1,21 @@
 import { Box, Typography } from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import ComplaintCard from '../components/ComplaintCard';
+import { useQuery } from "@tanstack/react-query";
+import api from "../api";
+
 
 const Complaints = () => {
-  //get data from api
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: async () => {
+      const result = await api.get('http://localhost:4000/api/complaints')
+      return result.data;
+    }
+  })
+  if (isPending) return 'Loading...'
+
+  if (error) return 'An error has occurred: ' + error.message
   return (
     <Box>
       <Typography
@@ -19,15 +31,19 @@ const Complaints = () => {
         container
         spacing={2}
         sx={{
-          maxHeight: "100vh", 
-          overflowY: "auto", 
+          maxHeight: "100vh",
+          overflowY: "auto",
         }}
       >
-        {dummyComplaints.map((complaint) => (
-          <Grid size={{ xs:12, sm:6, md:4, lg:3 }} key={complaint.id}>
-            <ComplaintCard complaint={complaint} />
-          </Grid>
-        ))}
+        {data?.length > 0 ? (
+          data.map((complaint) => (
+            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} key={complaint.id}>
+              <ComplaintCard complaint={complaint} />
+            </Grid>
+          ))
+        ) : (
+          <Typography>No complaints found</Typography>
+        )}
       </Grid>
     </Box>
   );
