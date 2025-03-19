1a46629e7ed7a67d581ac295f73a1081c1d2934d
johnmdrake1 <32374869+johnmdrake1@users.noreply.github.com>
2025-03-19 02:00:19 -0500

    Details component done, but I think I'm going to change to use MUI grid.

---
+++ front-end/src/components/aptdetails/Details.jsx
@@ -0,0 +1,41 @@
+import { Box, Typography, Divider } from "@mui/material";
+import React from "react";
+
+const Details = ({ apartmentData }) => {
+
+    const {
+        layout,
+        squareFootage,
+        bedrooms,
+        bathrooms,
+        floor,
+    } = apartmentData;
+
+
+    return (
+        <Box
+          sx={{
+            width: "33.3%",
+            border: "1px solid #ccc",
+            borderRadius: 1,
+            p: 2,
+            display: "flex",
+            flexDirection: "column"
+          }}
+        >
+            <Typography variant="h2" sx={{ fontWeight: "bold", mb: 1 }}>
+                Details
+            </Typography>
+
+            <Divider orientation="horizontal" flexItem />
+
+            <Typography variant="h4">Layout: {layout}</Typography>
+            <Typography variant="h4">Sq. Ft: {squareFootage}</Typography>
+            <Typography variant="h4">Beds: {bedrooms}</Typography>
+            <Typography variant="h4">Baths: {bathrooms}</Typography>
+            <Typography variant="h4">Floor: {floor}</Typography>
+        </Box>
+    )
+}
+
+export default Details;
