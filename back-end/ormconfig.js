854b2c793168f68bc0a723e1871ee56a70682191
Brian Angulo <bangulo219@gmail.com>
2025-03-20 18:13:35 -0400

    db-ci: integrating environment variables

--- back-end/ormconfig.js
+++ back-end/ormconfig.js
@@ -5,14 +5,17 @@ const Apartment = require("./database/entities/apartment");
 const Tenant = require("./database/entities/tenant");
 const Complaint = require("./database/entities/complaint");
 
+const { DB_USER, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_DATABASE } =
+  process.env;
+
 module.exports = {
-  name: "default",
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "password",
-  database: "tenant_portal",
+  user: DB_USER,
+  host: DB_HOST,
+  port: Number(DB_PORT),
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
   synchronize: false,
   logging: false,
   entities: [Admin, Lease, Apartment, Tenant, Complaint],
