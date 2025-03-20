1cb649a19d18ddda41dc6bfa35834f77487853f7
Brian Angulo <bangulo219@gmail.com>
2025-03-20 18:55:46 -0400

    db-ci:  cors base

--- back-end/app.js
+++ back-end/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN_BASE || "http://localhost:5173",
     credentials: true,
   })
 );
