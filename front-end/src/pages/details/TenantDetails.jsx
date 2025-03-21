cf8dd5e4a160c98338e28f08b9e092bf847c19de
Tierney Allen <tierney_03@mac.com>
2025-03-20 19:34:17 -0500

    Corrected console.logs and added spinner for loading

--- front-end/src/pages/details/TenantDetails.jsx
+++ front-end/src/pages/details/TenantDetails.jsx
@@ -4,6 +4,7 @@ import TenantProfileDetails from "../../components/TenantProfileDetails";
 import { fetchTenantInformation } from "../../api/tenantApi";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Spinner from "../../components/Spinner";
 
 const TenantDetails = () => {
   const [tenant, setTenant] = useState(null);
@@ -14,20 +15,18 @@ const TenantDetails = () => {
     async function fetchTenantInfo() {
       try {
         const tenant = await fetchTenantInformation(id);
-        console.log("tenant", tenant);
         setTenant(tenant);
       } catch (err) {
         return err;
       }
     }
-    console.log("fetching");
     fetchTenantInfo(id);
   }, [id]);
 
   if (!tenant) {
-    return <div>Loading...</div>;
+    <Spinner />;
   }
-  console.log("tenant", tenant);
+
   return (
     <Grid2 container spacing={2} padding={2} className="min-h-screen">
       <Grid2 size={{ xs: 12, md: 6 }}>
